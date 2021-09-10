cd3354390a6613a3a38557dd9291e4230d7e658d
crisptaylor <79578973+crisptaylor@users.noreply.github.com>
2021-09-10 14:01:19 +0200

    added slide-in animations and fixed Mobile

--- assets/js/observer.js
+++ assets/js/observer.js
@@ -1,13 +1,14 @@
 const nav = document.querySelector("nav");
 const sectionOne = document.querySelector(".intro-section");
-
+const fader = document.querySelectorAll(".fade-in");
+const slider = document.querySelectorAll(".slide-in");
 const sectionOneOptions = {
     rootMargin:"-220px 0px 0px 0px"
 };
 
 const sectionOneObserver = new IntersectionObserver (function(
     entries,
-   sectionOneObserver
+   _sectionOneObserver
     ) {
        entries.forEach(entry => {
            if(!entry.isIntersecting) {
@@ -30,3 +31,29 @@ const sectionOneObserver = new IntersectionObserver (function(
 sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
+
+const appearoptions = {
+threshold:0,
+rootMargin:"0px 0px -200px 0px",
+};
+//NEW OBSERVER
+const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+    ) {
+        entries.forEach(entry=> {
+        if(!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+},
+appearoptions);
+fader.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+slider.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
